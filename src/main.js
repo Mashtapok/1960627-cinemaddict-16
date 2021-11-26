209b2b42a982c8408fda321b88b6da94cba214ff
Stepan <mashtakovstepan@yandex.ru>
2021-11-26 10:16:08 +0300

    Разбиваем вёрстку на компоненты

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import {RenderPosition, renderTemplate} from './render';
+import {createHeaderProfileTemplate} from './view/header-profile-view';
+import {createNavigationTemplate} from './view/navigation-view';
+import {createSortTemplate} from './view/sort-view';
+import {createContentTemplate} from './view/content-view';
+import {createFooterStatisticsTemplate} from './view/footer-statistics-view';
+import {createFilmCardTemplate} from './view/film-card-view';
+import {createLoadMoreButtonTemplate} from './view/load-more-button-view';
+import {createFilmCardExtraTemplate} from './view/film-card-extra-view';
+
+const siteMainElement = document.querySelector('.main');
+const siteHeaderElement = document.querySelector('.header');
+const siteFooterElement = document.querySelector('.footer');
+const siteFooterStatisticsElement = siteFooterElement.querySelector('.footer__statistics');
+
+
+renderTemplate(siteHeaderElement, createHeaderProfileTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSortTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(siteMainElement, createNavigationTemplate(), RenderPosition.AFTERBEGIN);
+renderTemplate(siteMainElement, createContentTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(siteFooterStatisticsElement, createFooterStatisticsTemplate(), RenderPosition.BEFOREEND);
+
+const filmsContainerElement = siteMainElement.querySelector('.films-list__container');
+const filmsListElement = siteMainElement.querySelector('.films-list');
+
+for (let i = 0; i < 5; i++) {
+  renderTemplate(filmsContainerElement, createFilmCardTemplate(), RenderPosition.AFTERBEGIN);
+}
+
+renderTemplate(filmsListElement, createLoadMoreButtonTemplate(), RenderPosition.BEFOREEND);
+
+for (let i = 0; i < 2; i++) {
+  renderTemplate(filmsListElement, createFilmCardExtraTemplate(), RenderPosition.AFTEREND);
+}
